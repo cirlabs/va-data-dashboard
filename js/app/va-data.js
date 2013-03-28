47afee77b77396110abff8c13ed36f50a0e94872
Shane shifflett <shaneshifflett@Shane-shiffletts-MacBook-Pro.local>
2013-03-28 15:39:09 -0700

    query data

---
+++ js/app/va-data.js
@@ -0,0 +1,33 @@
+try{VaDashboard = VaDashboard;}catch(err){VaDashboard = {};}
+VaDashboard.events = VaDashboard.events !== undefined ? VaDashboard.events : _.extend({}, Backbone.Events);
+VaDashboard.templates = VaDashboard.templates !== undefined ? VaDashboard.templates : {};
+var VaData = Backbone.Model.extend({
+    initialize: function(attributes){
+        this.attributes = attributes;
+    },
+    url: function(){
+        return this.attributes.resource_uri;
+    }
+});
+var VaDataCollection = Backbone.Collection.extend({
+    model: VaData,
+    initialize: function(models, options){
+        this.fieldType = options.fieldType;
+        this.resourceUri = options.resourceUri;
+    },
+    sync: function(method, model, options) {
+        var params = _.extend({
+            type: 'GET',
+            dataType: 'jsonp',
+            url: model.url(),
+            processData: true
+        }, options);
+        return $.ajax(params);
+    },
+    parse: function(response) {
+        return response.objects;
+    },
+    url: function(){
+        return this.resourceUri;
+    }
+});
